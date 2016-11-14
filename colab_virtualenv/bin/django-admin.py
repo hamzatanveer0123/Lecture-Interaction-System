ef4b7ce3247d0a7a1766cef0accfaa180e1894bd
hamzamalik0123 <hamzamalik0123@gmail.com>
2016-11-14 14:40:37 +0000

    fixed virtual env issues

---
+++ colab_virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hamzamalik0123/OneDrive/colab/colab_Project/colab/colab_virtualenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- colab_virtualenv/colab_virtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/hamzamalik0123/OneDrive/colab/colab_Project/colab/colab_project/colab_virtualenv/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
