b444931bbd6e4fc101d715e3a2262d5a8ed72ef9
hamzamalik0123 <hamzamalik0123@gmail.com>
2016-12-21 23:25:26 +0000

    new start

--- colab_project/colab_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from colab_app import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
--- colab_project/static/script/functions.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-    $(".student_question").click(function () {
-        $(".question_window").toggleClass("toggleQuestionWindowMargin");
-        $(".wrapper").toggleClass("toggleWrapperMargin");
-    });
-});
